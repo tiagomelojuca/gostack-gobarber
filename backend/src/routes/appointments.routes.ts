8d80d61fc1808e0ad5d40342588bc7182fd32e53
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-01-29 04:41:39 -0300

    Init route for new appointment

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const appointmentsRouter = Router();
+const appointments: Array<unknown> = [];
 
-appointmentsRouter.get('/', (req, res) =>
-  res.json({ message: 'Hello GoBarber!' }),
-);
+appointmentsRouter.get('/', (req, res) => res.json(appointments));
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return res.json(appointment);
+});
 
 export default appointmentsRouter;
