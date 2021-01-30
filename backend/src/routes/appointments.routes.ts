2a64e338d83e920c08293507ab203b3e5a55b202
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-01-30 08:54:58 -0300

    Install date-fns and validate book hour collision

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,18 +1,37 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
-const appointments: Array<unknown> = [];
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Array<Appointment> = [];
 
 appointmentsRouter.get('/', (req, res) => res.json(appointments));
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const foundAppointment = appointments.findIndex(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (foundAppointment > -1) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
