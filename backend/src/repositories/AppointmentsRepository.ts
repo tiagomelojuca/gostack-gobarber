3d231b158bee4a06341ed19ab723805f6bc004ce
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-01-30 09:38:46 -0300

    Code refactor creating repository for Appointments Our routes should have the less responsability as possible

---
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,26 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const foundAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return foundAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointments.push(appointment);
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,30 +1,26 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Array<Appointment> = [];
-
-appointmentsRouter.get('/', (req, res) => res.json(appointments));
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const foundAppointment = appointments.findIndex(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const foundAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
-  if (foundAppointment > -1) {
+  if (foundAppointmentInSameDate) {
     return res
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
