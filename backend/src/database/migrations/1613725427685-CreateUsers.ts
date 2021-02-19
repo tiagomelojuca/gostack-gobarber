4a37133b0a3a0312c4b3271bdd5f6485f445fec7
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-02-19 08:30:22 -0300

    Create relation between Appointments and Users

--- backend/src/database/migrations/1613725427685-CreateUsers.ts
+++ backend/src/database/migrations/1613725427685-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1613725427685 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
