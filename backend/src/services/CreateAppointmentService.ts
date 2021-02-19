0968743e088f026ff348aa786569fa956ac5bb85
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-02-19 08:58:05 -0300

    Change Appointments service/routes for new field

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -5,12 +5,15 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+  }: RequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
@@ -24,7 +27,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
