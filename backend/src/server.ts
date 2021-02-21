29c493446705293d11430fd42026b6509d2d10cf
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-02-21 04:50:54 -0300

    Serve uploads in a specific route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/uploadConfig';
 
 import './database';
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Server running on port 3333...');
